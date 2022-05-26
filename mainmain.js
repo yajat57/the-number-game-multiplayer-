57481e65f182b9fce0b93f619da4adbb2c0458f6
yajat57 <92434724+yajat57@users.noreply.github.com>
2022-05-26 16:52:37 +0530

    Add files via upload

---
+++ mainmain.js
@@ -0,0 +1,56 @@
+function adduser() {
+    player1_name=document.getElementById("player1_name_input").value;
+    player2_name=document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+
+
+    window.location="indexindex.html";
+}
+player1_name=localStorage.getItem("player1_name");
+player2_name=localStorage.getItem("player2_name");
+
+player1_score=0;
+player2_score=0;
+
+document.getElementById("player1_name").innerHTML=player1_name+":";
+document.getElementById("player2_name").innerHTML=player2_name+":";
+
+
+document.getElementById("player1_name").innerHTML=player1_score;
+document.getElementById("player2_name").innerHTML=player2_score;
+
+
+document.getElementById("player_question").innerHTML="question turn-"+player1_name;
+document.getElementById("player_answer").innerHTML="answer turn-"+player2_name;
+
+function send(){
+    get_word=document.getElementById("word").ariaValueMax;
+    word=get_word.toLowerCase();
+    console.log("word in LowerCase="+word);
+
+    charAt1=word.charAt(1);
+    console.log(charAt1);
+
+    lenght_divide_2=Math.floor(word.lenght/2);
+    charAt2=word.charAt(lenght_divide_2);
+    console.log(charAt2);
+
+    lenght_minus_1=word.lenght-1;
+    charAt3=word.charAt(lenght_minus_1);
+    console.log(charAt3);
+
+    remove_char1=word.replace(charAt1, "_");
+    remove_char2=remove_charAt1.replace(charAt2, "_");
+    remove_char3=remove_charAt2.replace(charAt3, "_");
+    console.log(remove_charAt3);
+
+    question_word="<h4 id='word_display'> q."+remove_charAt3+"</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row=question_word+input_box+check_button;
+    document.getElementById("output").innerHTML=row;
+    document.getElementById("word").value="";
+    
+}
